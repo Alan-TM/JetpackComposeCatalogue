6f7a18bf4b9a2490a27491ae1731a22663b764d4
Alan Torres Morales <alantorresm86@gmail.com>
2023-07-09 14:56:23 -0700

    feat: bottom nav is added

--- app/src/main/java/com/alan/jetpackcomposecatalogo2/Scaffold.kt
+++ app/src/main/java/com/alan/jetpackcomposecatalogo2/Scaffold.kt
@@ -1,5 +1,7 @@
 package com.alan.jetpackcomposecatalogo2
 
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
@@ -7,38 +9,115 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
 fun ScaffoldExample() {
-    Scaffold(topBar = { MyTopAppBar() }) {
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            MyTopAppBar { actionText ->
+                coroutineScope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar("Haz pulsado: $actionText")
+                }
+            }
+        },
+        scaffoldState = scaffoldState,
+        bottomBar = {
+            MyBottomNavigation()
+        }
+    ) {
     }
 }
 
 @Composable
-fun MyTopAppBar() {
+fun MyTopAppBar(
+    onClickIcon: (String) -> Unit
+) {
     TopAppBar(
         title = { Text(text = "Mi primera toolbar") },
         backgroundColor = Color.Red,
         contentColor = Color.White,
         elevation = 4.dp,
         navigationIcon = {
-            IconButton(onClick = {}) {
+            IconButton(onClick = { onClickIcon("Atrás") }) {
                 Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
             }
         },
         actions = {
-            IconButton(onClick = {}) {
+            IconButton(onClick = { onClickIcon("Buscar") }) {
                 Icon(imageVector = Icons.Filled.Search, contentDescription = "Search")
             }
 
-            IconButton(onClick = {}) {
+            IconButton(onClick = { onClickIcon("Estrella!") }) {
                 Icon(imageVector = Icons.Filled.Star, contentDescription = "Star")
             }
         }
     )
+}
+
+@Composable
+fun MyBottomNavigation() {
+    var index by remember { mutableStateOf(0) }
+    BottomNavigation(
+        backgroundColor = Color.Red,
+        contentColor = Color.White
+    ) {
+        BottomNavigationItem(
+            selected = index == 0,
+            onClick = { index = 0 },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = "Home"
+                )
+            },
+            label = {
+                Text(text = "Home")
+            }
+        )
+
+        BottomNavigationItem(
+            selected = index == 1,
+            onClick = { index = 1 },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = "Favorite"
+                )
+            },
+            label = {
+                Text(text = "Favorite")
+            }
+        )
+
+        BottomNavigationItem(
+            selected = index == 2,
+            onClick = { index = 2 },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Person"
+                )
+            },
+            label = {
+                Text(text = "Person")
+            }
+        )
+    }
 }
